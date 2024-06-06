027cde5b35b8d723ec3a92c3605af599675c1569
Johannsonmanson <johann.vasquez@usm.cl>
2024-06-05 20:52:39 -0400

    Added try-catch to endpoints

--- INF236-Users-Backend-Boilerplate-master/src/controllers/SimulController.js
+++ INF236-Users-Backend-Boilerplate-master/src/controllers/SimulController.js
@@ -1,37 +1,54 @@
 import Simul from '../models/simulaciones.js';
 
 export default class SimulController{
-    async getAll(req, res) {
-		const simul = await Simul.findAll();
-		res.send(simul);
+	async getAll(req, res) {
+		try {
+			const simul = await Simul.findAll();
+			res.send(simul);
+		} catch (error) {
+			res.status(500).send("Error");
+		}
 	}
 
-    async getByrut(req, res) {
-		const simul = await Simul.findAll({
-			where: {
-				rut: req.params.rut
-			}
-		});
-		res.send(simul);
+	async getByrut(req, res) {
+		try {
+			const simul = await Simul.findAll({
+				where: {
+					rut: req.params.rut
+				}
+			});
+			res.send(simul);
+			
+		} catch (error) {
+			res.status(500).send("Error");
+		}
 	}
 
-    async get(req, res) {
-		const simul = await Simul.findByPk(req.params.id);
-		res.send(simul);
+	async get(req, res) {
+		try {
+			const simul = await Simul.findByPk(req.params.id);
+			res.send(simul);
+		} catch (error) {
+			res.status(500).send("Error");
+		}
 	}
 
-    async create(req, res) {
-		const simul = await Simul.create({
-			rut: req.body.rut,
-			fecha: req.body.fecha,
-			monto: req.body.monto_prestamo,
-			n_cuotas : req.body.n_cuotas,
-            UF : req.body.UF,
-			interes: req.body.interes,
-            Total_UF: req.body.Total_UF,
-            Cuota_UF: req.body.precio_cuota,
-		});
-		res.send(simul);
+	async create(req, res) {
+		try {
+			const simul = await Simul.create({
+				rut: req.body.rut,
+				fecha: req.body.fecha,
+				monto: req.body.monto_prestamo,
+				n_cuotas : req.body.n_cuotas,
+							UF : req.body.UF,
+				interes: req.body.interes,
+							Total_UF: req.body.Total_UF,
+							Cuota_UF: req.body.precio_cuota,
+			});
+			res.send(simul);
+		} catch (error) {
+			res.status(500).send("Error");
+		}
 	}
 
 }
--- INF236-Users-Backend-Boilerplate-master/src/controllers/SolicitudController.js
+++ INF236-Users-Backend-Boilerplate-master/src/controllers/SolicitudController.js
@@ -3,36 +3,51 @@ import { Sequelize } from 'sequelize';
 
 export default class SolicitudController{
 	async getAll(req, res) {
-		const soli = await Solicitud.findAll();
-		console.log(soli);
-		res.send(soli);
+		try {
+			const soli = await Solicitud.findAll();
+			console.log(soli);
+			res.send(soli);
+		} catch (error) {
+			res.status(500).send("Error");
+		}
 	}	
 
 	async getBynombre(req, res) {
-		const soli = await Solicitud.findAll({
-			where: {
-				nombre: req.params.nombre
-			}
-		});
-		res.send(soli);
+		try {
+			const soli = await Solicitud.findAll({
+				where: {
+					nombre: req.params.nombre
+				}
+			});
+			res.send(soli);
+		} catch (error) {
+			res.status(500).send("Error");
+		}
 	}
 
 	async get(req, res) {
-		const soli = await Solicitud.findByPk(req.params.id);
-		res.send(soli);
+		try {
+			const soli = await Solicitud.findByPk(req.params.id);
+			res.send(soli);
+		} catch (error) {
+			res.status(500).send("Error");
+		}
 	}
 
 	async getByEjecutivoId(req, res) {
-		const id = req.params.userId;
-		const solicitudes = await Solicitud.findAll({
-			where: {
-				created_by: id
-			},
-			attributes: ['id', 'nombre', 'fecha', 'rut', 'estado', 'derivada']
-		});
-		const solicitudesDataValues = solicitudes.map(solicitud => solicitud.dataValues);
-		res.send(solicitudesDataValues);
-		///
+		try {
+			const id = req.params.userId;
+			const solicitudes = await Solicitud.findAll({
+				where: {
+					created_by: id
+				},
+				attributes: ['id', 'nombre', 'fecha', 'rut', 'estado', 'derivada']
+			});
+			const solicitudesDataValues = solicitudes.map(solicitud => solicitud.dataValues);
+			res.send(solicitudesDataValues);
+		} catch (error) {
+			res.status(500).send("Error");
+		}
 	}
 
 	async getAcceptedSolicitudbyDateRange(req,res) {
@@ -56,43 +71,62 @@ export default class SolicitudController{
 	}
 
 	async create(req, res) {
-		//console.log(req.body.userId, 'aaaaa');
-		const soli = await Solicitud.create({
-			created_by: req.body.userId,
-			nombre: req.body.nombre,	
-			rut: req.body.rut,
-			fecha: req.body.fecha,
-			cargo: req.body.oficio,
-			tipo_prestamo: req.body.tipo_prestamo,
-			monto_total: req.body.monto_prestamo,
-			precio_cuota: req.body.precio_cuota,
-			n_cuotas : req.body.n_cuotas,
-			motivo: req.body.motivo,
-			estado: req.body.estado,
-			derivada: req.body.derivada,
-			interes: req.body.interes,
-		});
-		res.send(soli);
+		try {
+			const soli = await Solicitud.create({
+				created_by: req.body.userId,
+				nombre: req.body.nombre,	
+				rut: req.body.rut,
+				fecha: req.body.fecha,
+				cargo: req.body.oficio,
+				tipo_prestamo: req.body.tipo_prestamo,
+				monto_total: req.body.monto_prestamo,
+				precio_cuota: req.body.precio_cuota,
+				n_cuotas : req.body.n_cuotas,
+				motivo: req.body.motivo,
+				estado: req.body.estado,
+				derivada: req.body.derivada,
+				interes: req.body.interes,
+			});
+			res.send(soli);
+		} catch (error) {
+			res.status(500).send("Error");
+		}
 	}
 	async update(req, res) {
-		const soli = await Solicitud.findByPk(req.params.id);
-		soli.update({derivada: 1});
-		res.send(soli);
+		try {
+			const soli = await Solicitud.findByPk(req.params.id);
+			soli.update({derivada: 1});
+			res.send(soli);
+		} catch (error) {
+			res.status(500).send("Error");
+		}
 	}
 	async aprobar(req, res) {
-		const soli = await Solicitud.findByPk(req.params.id);
-		soli.update({estado: 1});
-		res.send(soli);
+		try {
+			const soli = await Solicitud.findByPk(req.params.id);
+			soli.update({estado: 1});
+			res.send(soli);
+		} catch (error) {
+			res.status(500).send("Error");
+		}
 	}
 	async rechazar(req, res) {
-		const soli = await Solicitud.findByPk(req.params.id);
-		soli.update({estado: 2});
-		res.send(soli);
+		try {
+			const soli = await Solicitud.findByPk(req.params.id);
+			soli.update({estado: 2});
+			res.send(soli);
+		} catch (error) {
+			res.status(500).send("Error");
+		}
 	}
 
 	async delete(req, res) {
-		await Solicitud.destroy({where: {id: req.params.id}});
-		res.send({status: "ok"});
+		try {
+			await Solicitud.destroy({where: {id: req.params.id}});
+			res.send({status: "ok"});
+		} catch (error) {
+			res.status(500).send("Error");
+		}
 	}	
 };
 
